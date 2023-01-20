b4f30d7eaec3ed9a45bf900a0da5db490696a2af
ympark99 <ympark1230@naver.com>
2023-01-20 15:20:47 +0900

    [CHORE] swagger 설정 완료

---
+++ src/main/java/com/mews/mews_backend/global/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.mews.mews_backend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    private static final String API_NAME = "Mews API";
+    private static final String API_VERSION = "0.0.1";
+    private static final String API_DESCRIPTION = "Mews API 명세서";
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(true)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.mews.mews_backend"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION)
+                .build();
+    }
+
+
+}
