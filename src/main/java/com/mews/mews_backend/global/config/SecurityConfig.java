db06ff408a664158d7df7bff81e2eb4a40bf3e3c
ympark99 <ympark1230@naver.com>
2023-02-07 18:20:31 +0900

    [CHORE] security 토큰 예외 uri 추가

--- src/main/java/com/mews/mews_backend/global/config/SecurityConfig.java
+++ src/main/java/com/mews/mews_backend/global/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -60,6 +61,17 @@ public class SecurityConfig  {
 
 
 
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().antMatchers(
+                "/v3/api-docs",
+                "/swagger-resources/**",
+                "/swagger-ui/**",
+                "/swagger/**",
+                "/"
+        );
     }
 
     @Bean
