9a142b812eb17d19fe665c6ae4ece192e5b8c126
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-02-23 17:25:01 +0900

    [FIX] 검색 쿼리 수정

--- src/main/java/com/mews/mews_backend/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/mews/mews_backend/domain/article/repository/ArticleRepository.java
@@ -3,6 +3,7 @@ package com.mews.mews_backend.domain.article.repository;
 import com.mews.mews_backend.api.article.dto.res.GetArticleRes;
 import com.mews.mews_backend.domain.article.entity.Article;
 import com.mews.mews_backend.domain.article.entity.Views;
+import com.mews.mews_backend.domain.editor.entity.Editor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -23,7 +24,7 @@ public interface ArticleRepository extends JpaRepository<Article, Integer> {
             "left join editor e on aae.editor_id = e.editor_id " +
             "where e.name like %:searchKeyword% " +
             "union " +
-            "select * " +
+            "select a.id, a.created_at, a.modified_at, a.content, a.file_urls, a.like_count, a.title, a.type, a.views_id " +
             "from article a " +
             "where a.title like %:searchKeyword%", nativeQuery = true)
     List<Article> findAllByKeyword(@Param("searchKeyword") String keyword);
@@ -33,4 +34,5 @@ public interface ArticleRepository extends JpaRepository<Article, Integer> {
     List<Article> findAllByType(String type);
 
     List<Article> findTop5ByOrderByViewsViewsDesc();
+
 }
