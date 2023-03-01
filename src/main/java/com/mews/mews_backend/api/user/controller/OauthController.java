61cdaa26982632a952f93d864e0bb4d153971428
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-03-01 13:47:58 +0900

    [FIX] 콜백 url + cross origin origins 수정

--- src/main/java/com/mews/mews_backend/api/user/controller/OauthController.java
+++ src/main/java/com/mews/mews_backend/api/user/controller/OauthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 @Slf4j
