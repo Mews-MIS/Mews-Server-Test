feeddb792d8149aaa13211b381bce072c1ae0085
Jinwoo Chang <jenny010925@naver.com>
2023-04-03 22:44:03 +0900

    [FEAT] 프로필 정보 반환시 정보 공개여부 추가

--- src/main/java/com/mews/mews_backend/global/config/RedisSearchConfig.java
+++ src/main/java/com/mews/mews_backend/global/config/RedisSearchConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
